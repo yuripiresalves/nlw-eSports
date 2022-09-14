61c1c33d2dc1da5bc21a17ae1456b6e2489137fd
Yuri Alves <yuripiresalves@gmail.com>
2022-09-14 16:59:06 -0300

    aula 3

--- server/src/server.ts
+++ server/src/server.ts
@@ -8,6 +8,7 @@ import { convertMinutesToHourString } from './utils/convert-minutes-to-hour-stri
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 const prisma = new PrismaClient({
   log: ['query'],
